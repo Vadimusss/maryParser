786f0b03971b27f524890aeda6222382d7950900
Vadim <v_a_d@inbox.ru>
2023-10-25 21:39:42 +0300

    update

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ import { generateSubUrls, makeUniqIdSet } from './utils.js';
 import { JSDOM } from 'jsdom';
 import _ from 'lodash';
 
-function* generatePagesLiks() {
+const regionsId = [
+  /*31, 32, 33, 36, */77, /* 37, 40, 44, 46, 48, 50, 57, 62, 67, 68, 69, 71,
+  76, 30, 34, 92, 23, 1, 8, 91, 61, 45, 66, 72, 86, 74, 89, 28, 79,
+  41, 49, 25, 14, 65, 27, 87, 22, 75, 38, 42, 24, 54, 55, 4, 3, 17,
+  19, 70, 7, 9, 5, 6, 15, 26, 20, 43, 52, 56, 58, 59, 2, 12, 13, 16,
+  63, 64, 18, 73, 21, 29, 35, 78, 39, 47, 51, 83, 53, 60, 10, 11,*/
+]
+
+const currentParseUrl = 'https://www.roi.ru/poll/?level=2,3&archive=1';
+
+function* generatePagesLiks(numberOfPages) {
   let page = 1;
-  const numberOfPages = 950;
 
   while (page <= numberOfPages) {
-    yield `https://www.roi.ru/poll/?level=2,3&archive=1${page}`;
+    yield `${currentParseUrl}&page=${page}`;
     page++;
   }
 }
@@ -29,10 +38,10 @@ const fetchInitiativeData = async (url) => {
   return [number, level, name, affirmative, negative, url, date];
 };
 
-const getInitiativesLinks = async (url, base) => {
+const getInitiativesLinks = async (url, base, regionId) => {
   const response = await axios.get(url, {
     headers: {
-      'Cookie': 'regionId=46',
+      'Cookie': `regionId=${regionId}`,
     }
   });
   const document = new JSDOM(response.data).window.document;
@@ -41,22 +50,38 @@ const getInitiativesLinks = async (url, base) => {
   return urls;
 };
 
-const run = async () => {
-  const urlGenerator = generatePagesLiks();
-  for (let url of urlGenerator) {
-    try {
-      const initiativesLinks = await getInitiativesLinks(url, 'https://www.roi.ru');
-      const initiativesData = await Promise.all(initiativesLinks.map(async (link) => await fetchInitiativeData(link)));
-
-      initiativesData.forEach((initiativeDataArr) => {
-        const dataString = initiativeDataArr.join('~');
-        console.log(dataString);
-        fs.appendFileSync('result.csv', `\n${dataString}`);
-      });
-    } catch (error) {
-      console.log(`error message in catch ===> ${error.message}`);
-      fs.appendFileSync('errors.csv', `\n\'in catch => ${error.message}\',`);
+const getNumberOfPages = async (regionId) => {
+  const response = await axios.get(currentParseUrl, {
+    headers: {
+      'Cookie': `regionId=${regionId}`,
     }
+  });
+  const document = new JSDOM(response.data).window.document;
+  const numbersOfPages = await Promise.all([...document.querySelectorAll('[data-page]:not(.next)')].map((element) => Number(element.textContent))); 
+
+  return (numbersOfPages.length !== 0) ? Math.max(...numbersOfPages) : 1;
+};
+
+const run = async () => {
+  try {
+    for (const regionId of regionsId) {
+      const numberOfPages = await getNumberOfPages(regionId);
+
+      const urlGenerator = generatePagesLiks(numberOfPages);
+      for (let url of urlGenerator) {
+        const initiativesLinks = await getInitiativesLinks(url, 'https://www.roi.ru', regionId);
+        const initiativesData = await Promise.all(initiativesLinks.map(async (link) => await fetchInitiativeData(link)));
+
+        initiativesData.forEach((initiativeDataArr) => {
+          const dataString = initiativeDataArr.join('~');
+          console.log(dataString);
+          fs.appendFileSync('result.csv', `\n${dataString}`);
+        });
+      }
+    }
+  } catch (error) {
+    console.log(`error message in catch ===> ${error.message}`);
+    fs.appendFileSync('errors.csv', `\n\'in catch => ${error.message}\',`);
   }
 };
 
